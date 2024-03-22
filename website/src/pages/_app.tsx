630f2cde30d66ab52b59c6ff3ad5581e4320738c
Joona Kytöniemi <jnkyto@utu.fi>
2024-03-22 15:30:51 +0200

    fix: still trying to fix og

--- website/src/pages/_app.tsx
+++ website/src/pages/_app.tsx
@@ -40,6 +40,7 @@ function MyApp({ Component, pageProps: { session, ...pageProps } }: AppPropsWith
 
   const metaTitle = "Avoin Avustaja | Keskusteleva tekoäly jokaiselle";
   const metaUrl = "https://avoin-avustaja.fi/";
+  const metaImage = "/images/opengraph/opengraph-image.jpg";
 
   return (
     <>
@@ -49,9 +50,13 @@ function MyApp({ Component, pageProps: { session, ...pageProps } }: AppPropsWith
 
         {/* Facebook Meta */}
         <meta property="og:url" content={metaUrl} />
+        <meta property="og:locale" content="fi_FI" />
         <meta property="og:type" content="website" />
         <meta property="og:title" content={metaTitle} />
+        <meta property="og:site_name" content="Avoin Avustaja" />
         <meta property="og:description" content={t("index:description")} />
+        <meta property="og:image" content={metaImage} />
+        <meta property="og:image:type" content="image/jpeg" />
 
         {/* X Meta */}
         <meta name="twitter:card" content="summary_large_image" />
@@ -59,6 +64,7 @@ function MyApp({ Component, pageProps: { session, ...pageProps } }: AppPropsWith
         <meta name="twitter:url" content={metaUrl} />
         <meta name="twitter:title" content={metaTitle} />
         <meta name="twitter:description" content={t("index:description")} />
+        <meta name="twitter:image" content={metaImage} />
       </Head>
       <FlagsProvider value={flags}>
         <Chakra>
